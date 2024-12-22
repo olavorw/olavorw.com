7ebd3685e5e9059f5481ed2805e1824457a968bb
Olav <73853649+olavorw@users.noreply.github.com>
2024-12-22 15:13:50 -0800

    Update logo assets and add new dependencies/features folder
    Replaces old logo images with updated assets for better clarity and branding. Adds `@headlessui/react` and related dependencies to `package.json` and expands Tailwind CSS configuration to include the `features` folder.

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -22,15 +22,15 @@ export function Logo() {
             <span className="sr-only">4934</span>
             <div className="relative w-20 h-20 overflow-hidden">
                 <Image
-                    src="/Logo/4934sqw.png"
-                    alt="4934 Logo"
+                    src="/Logo/MainSquareTransparent.png"
+                    alt="Olavorw Logo"
                     width={80}
                     height={80}
                     className="absolute top-0 left-0 transition-opacity duration-200 ease-in-out group-hover:opacity-0"
                 />
                 <Image
-                    src="/Logo/4934sqg.png"
-                    alt="4934 Logo"
+                    src="/Logo/GradientSquareGlow.png"
+                    alt="Olavorw Gradient Logo"
                     width={80}
                     height={80}
                     className="absolute top-0 left-0 transition-opacity duration-200 ease-in-out opacity-0 group-hover:opacity-100"
