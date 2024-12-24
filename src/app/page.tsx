5ffa585d6beb5ded4d50b6138c7b675475a62e2d
Olav <73853649+olavorw@users.noreply.github.com>
2024-12-24 10:06:59 -0800

    Remove unused CallToAction and ContactForm components.
    The CallToAction and ContactForm components were removed from the Home page since they are no longer needed. This simplifies the code and removes unnecessary imports, reducing potential maintenance overhead.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,9 @@
 import HomeHero from '@/features/HomeHero/components';
-import CallToAction from '@/components/CallToAction';
-import ContactForm from '../features/ContactForm/components';
 
 export default function Home() {
   return (
     <>
       <HomeHero />
-      <CallToAction
-        imageHeight={400}
-        imageWidth={600}
-        imageSource="https://placehold.co/600x400/black/gray/png"
-        description={
-          "This is my super duper awesome CTA component! It's so cool, you won't believe it!"
-        }
-        tagline={'Call To Action'}
-        buttonText={'Cool Button'}
-        buttonLink={'#'}
-      />
-      <ContactForm />
     </>
   );
 }
