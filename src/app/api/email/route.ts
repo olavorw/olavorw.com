f2da9992331464e34ee772248d5519ef207a9360
Olav <73853649+olavorw@users.noreply.github.com>
2024-12-24 11:26:20 -0800

    RAHHH CLOUDFLARE SUCKS

--- src/app/api/email/route.ts
+++ src/app/api/email/route.ts
@@ -11,7 +11,7 @@ export async function POST(request: NextRequest) {
     const recipientEmails = process.env.RECIPIENT_EMAILS || '';
     const mailgunSender = process.env.MAILGUN_SENDER_EMAIL || '';
 
-    const from = `Contact Olav // Olavorw <${mailgunSender}>`;
+    const from = `Contact Olav "Olavorw" <${mailgunSender}>`;
     const subject = `${firstName} ${lastName} at ${company}, ${email} - Olavorw Contact Form Submission`;
     const bodyText = `${message}\n\nThis message was sent from the contact form on olavorw.com in accordance with the privacy policy (https://olavorw.com/legal/policies/privacy).`;
 
@@ -43,8 +43,12 @@ export async function POST(request: NextRequest) {
 
     return NextResponse.json({ status: 200 });
   } catch (error: unknown) {
+    console.error('Detailed error:', error);
     if (error instanceof Error) {
-      return NextResponse.json({ error: error.message }, { status: 500 });
+      return NextResponse.json(
+        { error: error.message, stack: error.stack },
+        { status: 500 }
+      );
     }
     return NextResponse.json({ error: String(error) }, { status: 500 });
   }
