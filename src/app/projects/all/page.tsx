c6f1d1f006b98f1b2b3c786e1b38c03159a4163a
Olav <73853649+olavorw@users.noreply.github.com>
2024-12-25 18:19:53 -0800

    Refactor AllProjects page to enhance content and structure
    Updated descriptions and icons, added construction notice, and introduced dropdown details for projects including features and links. These changes improve usability and provide clearer information about the projects.

--- src/app/projects/all/page.tsx
+++ src/app/projects/all/page.tsx
@@ -1,13 +1,24 @@
 import UnifiedSection from '@/components/UnifiedSection';
 import EpicCards from '@/components/EpicCards';
-import { AppWindowIcon, Home, Users } from 'lucide-react';
+import {
+  AppWindowIcon,
+  BrainCircuit,
+  Code,
+  Cog,
+  Home,
+  Mic,
+  User,
+  Users,
+} from 'lucide-react';
 
 export default function AllProjects() {
   return (
     <>
       <UnifiedSection
         title={'Projects'}
-        description={'These are all of my major projects, as cards!'}
+        description={
+          'These are all of my major projects, as cards! This page is currently under construction. Check back later for updates!'
+        }
         centered={true}
         tagline={'All Projects'}
       />
@@ -18,6 +29,9 @@ export default function AllProjects() {
             description:
               '4934, a transparent, open source, friendly community organization for makers.',
             icon: <Users />,
+            linkText: 'Project Page',
+            linkHref: 'https://olavorw.com/projects/4934',
+            dropdownText: 'About this project',
           },
           {
             title: 'AutoHome',
@@ -26,12 +40,43 @@ export default function AllProjects() {
             icon: <Home />,
             linkText: 'Project Page',
             linkHref: 'https://olavorw.com/projects/autohome',
+            dropdownText: 'About AutoHome',
+            features: [
+              {
+                feature: 'OpenAI',
+                featureDescription: 'GPT models control your home.',
+                featureIcon: <BrainCircuit />,
+              },
+              {
+                feature: 'Conversational',
+                featureDescription: 'Talk to your friend.',
+                featureIcon: <Mic />,
+              },
+              {
+                feature: 'Smart',
+                featureDescription: 'Unlimited Integration.',
+                featureIcon: <Cog />,
+              },
+              {
+                feature: 'ElevenLabs',
+                featureDescription: 'Insanely speedy, realistic voice.',
+                featureIcon: <User />,
+              },
+              {
+                feature: 'Open Source',
+                featureDescription: 'Completely open source.',
+                featureIcon: <Code />,
+              },
+            ],
           },
           {
             title: '4934.tech',
             description:
               'The official website for the 4934 community organization.',
             icon: <AppWindowIcon />,
+            linkText: 'Project Page',
+            linkHref: 'https://4934.tech',
+            dropdownText: 'About 4934.tech',
           },
         ]}
       />
