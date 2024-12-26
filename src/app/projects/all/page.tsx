d0984d6b7da0ae3a4d66438dc2c56bbfbc9d9750
Olav <73853649+olavorw@users.noreply.github.com>
2024-12-25 18:09:05 -0800

    Update "All Projects" page with project cards
    Replaces placeholder content with a list of project cards showcasing key projects. Introduces the `EpicCards` component to display project details, including titles, descriptions, icons, and links where applicable. Enhances the page with a more informative and visually appealing layout.

--- src/app/projects/all/page.tsx
+++ src/app/projects/all/page.tsx
@@ -1,14 +1,40 @@
 import UnifiedSection from '@/components/UnifiedSection';
+import EpicCards from '@/components/EpicCards';
+import { AppWindowIcon, Home, Users } from 'lucide-react';
 
 export default function AllProjects() {
   return (
-    <UnifiedSection
-      title={'Under Construction'}
-      description={
-        'This page is currently under construction. Check back later for updates!'
-      }
-      centered={true}
-      tagline={'All Projects'}
-    />
+    <>
+      <UnifiedSection
+        title={'Projects'}
+        description={'These are all of my major projects, as cards!'}
+        centered={true}
+        tagline={'All Projects'}
+      />
+      <EpicCards
+        cards={[
+          {
+            title: '4934',
+            description:
+              '4934, a transparent, open source, friendly community organization for makers.',
+            icon: <Users />,
+          },
+          {
+            title: 'AutoHome',
+            description:
+              'AutoHome, an AI to control your home, an automation system for the modern age.',
+            icon: <Home />,
+            linkText: 'Project Page',
+            linkHref: 'https://olavorw.com/projects/autohome',
+          },
+          {
+            title: '4934.tech',
+            description:
+              'The official website for the 4934 community organization.',
+            icon: <AppWindowIcon />,
+          },
+        ]}
+      />
+    </>
   );
 }
