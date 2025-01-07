fc8cc164b3d22980b33d4690c1f41b8e16a1aee7
Olav <73853649+olavorw@users.noreply.github.com>
2025-01-06 17:13:50 -0800

    Fixing Vercel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,8 +7,6 @@ import NavBar from '@/features/NavBar/components';
 import DynamicBackground from '../features/CyberBackground/components';
 import Footer from '@/features/Footer/components';
 import React from 'react';
-import { Analytics } from '@vercel/analytics/react';
-import { SpeedInsights } from '@vercel/speed-insights/next';
 
 {
   /* import Banner from "@/components/Banner"; */
@@ -115,8 +113,6 @@ export default function RootLayout({
         */}
         <DynamicBackground />
         <main className="flex-grow pt-16 z-10 relative">{children}</main>
-        <Analytics />
-        <SpeedInsights />
         <Footer />
       </body>
     </html>
